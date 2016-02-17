95cccafec0898553654acdb1bc529be200af567b
Heitor Kuba <hjkuba@yahoo.com.br>
2016-02-17 17:57:47 -0200

    Botões e opções na tela de evento

---
+++ view/js/scripts-admin.js
@@ -0,0 +1,13 @@
+/*window.onload=function() {
+	var palestras = $(".lista_palestras .list-group-item");	
+	var btn = $(".add-palestra");	
+	
+	for(var i = 0; i<palestras.length; i++) {		
+		palestras[i].onmouseover = function() {
+			$(".add-palestra")[i].style.display = "block";						
+		}
+		palestras[i].onmouseout = function() {
+			$(".add-palestra")[i].style.display = "none";							
+		}
+	}
+}*/
