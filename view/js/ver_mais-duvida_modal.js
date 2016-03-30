16f8fe2080291fc08785694edd1ae203fe352c7f
Heitor Kuba <hjkuba@yahoo.com.br>
2016-03-30 16:12:54 -0300

    Alterações no modal de resposta do professor

--- view/js/ver_mais-duvida_modal.js
+++ view/js/ver_mais-duvida_modal.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     var showChar = 200;
-    var ellipsestext = "...";    
+    var ellipsestext = "...";
     $('.more').each(function() {
-        var content = $(this).html(); 
+        var content = $(this).html();
 
-        if(content.length > showChar) { 
+        if(content.length > showChar) {
             var c = content.substr(0, showChar);
-            var h = content.substr(showChar-1, content.length - showChar); 
+            var h = content.substr(showChar-1, content.length - showChar);
             var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span>';
-            $(this).html(html); 
-        }                      
+            $(this).html(html);
+        }
     });
 });
