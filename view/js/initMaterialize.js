ca5e7d526576b33c8eb44581dd401ce6d753b5c4
Heitor Kuba <hjkuba@yahoo.com.br>
2016-05-13 08:45:40 -0300

    Barra lateral

--- view/js/initMaterialize.js
+++ view/js/initMaterialize.js
@@ -1,10 +1,10 @@
+$(".button-collapse").sideNav();
 $(document).ready(function(){
   $('.collapsible').collapsible({
     accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
   });
   $('ul.tabs').tabs();
   $('.modal-trigger').leanModal();
-  $(".button-collapse").sideNav();
 });
 
 
