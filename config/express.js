41fddc9aaef4d7bc20f1859847fac0493e730752
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2016-08-03 16:51:56 -0300

    Middleware para redirecionar usuários ja logados

--- config/express.js
+++ config/express.js
@@ -18,6 +18,67 @@ module.exports = function () {
   app.use(bodyParser.json());
   app.use(require('method-override')());
   app.use(cookieParser());
+  // Auth middleware
+  // app.use(function (req, res, next) {
+  //   if (req.url == '/') {
+  //     next();
+  //     return;
+  //   }
+  //
+  //   if (!req.cookies.session || !req.cookies.session.token) {
+  //     res.redirect('/');
+  //     return;
+  //   }
+  //
+  //   var token = req.cookies.session.token;
+  //   request(
+  //       {
+  //         uri: app.utils.constants.ws.uri + '/session',
+  //         method: 'GET',
+  //         headers: {
+  //           'x-session-token': token
+  //         }
+  //       }, function (error, response, body) {
+  //         if (response.statusCode == 401) {
+  //           res.status(401);
+  //           res.end();
+  //         } else {
+  //           next();
+  //         }
+  //       }
+  //   );
+  // });
+  // Redirect middleware
+  app.use(function (req, res, next) {
+    if (req.url != '/') {
+      next();
+      return;
+    }
+
+    if (!req.cookies.session || !req.cookies.session.token) {
+      next();
+      return;
+    }
+
+    var token = req.cookies.session.token;
+    request(
+        {
+          uri: app.utils.constants.ws.uri + '/session',
+          method: 'GET',
+          headers: {
+            'x-session-token': token
+          }
+        }, function (error, response, body) {
+          if (response.statusCode == 401) {
+            res.redirect('/');
+            res.end();
+          } else {
+            res.redirect('/home');
+            next();
+          }
+        }
+    );
+  });
 
   //express load
   load('utils', {cwd: 'app'})
