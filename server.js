102bf3dc42eefc2ee68c2349eae31b71125e9297
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2016-08-23 14:23:05 -0300

    Adiciona estrutura básica de diretórios do Express

--- server.js
+++
@@ -1,7 +0,0 @@
-var app = require('./config/express')();
-var http = require('http').Server(app);
-var socket = require('./app/socket/socket')(http);
-
-http.listen(app.get('port'), function () {
-  console.log('Oddin rodando na porta ' + app.get('port'));
-});
