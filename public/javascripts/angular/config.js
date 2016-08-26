6497a3f0ad0f7527bbd38f14676ced07e9708333
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2016-08-26 12:04:11 -0300

    Convert HTML to Pug

--- public/javascripts/angular/config.js
+++ public/javascripts/angular/config.js
@@ -1,86 +1,86 @@
-oddin.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider
+oddin.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
         .state('disciplinas', {
-            url: "/disciplinas",
-            templateUrl: "partials/disciplinas.html",
-            controller: "DisciplinasController"
+          url: '/disciplinas',
+          templateUrl: '/partials/disciplinas',
+          controller: 'DisciplinasController',
         })
         .state('aulas', {
-            url: "/disciplinas/:disciplinaID/aulas",
-            templateUrl:"partials/aulas.html"
+          url: '/disciplinas/:disciplinaID/aulas',
+          templateUrl: '/partials/aulas',
         })
             .state('aulas.aluno', {
-                url: "/aluno",
-                templateUrl:'partials/aulas-a.html',
-                controller: "DisciplinaController"
+              url: '/aluno',
+              templateUrl: '/partials/aulas-a',
+              controller: 'DisciplinaController',
             })
             .state('aulas.professor', {
-                url: "/professor",
-                templateUrl:'partials/aulas-p.html',
-                controller: "DisciplinaController"
+              url: '/professor',
+              templateUrl: '/partials/aulas-p',
+              controller: 'DisciplinaController',
             })
         .state('materiais', {
-            url: "/disciplinas/:disciplinaID/materiais",
-            templateUrl:"partials/materiais.html",
-            controller: function($cookies, $state) {
-                if ($cookies.get('profile') == 0) {
-                    $state.go('materiais.aluno');
-                } else {
-                    $state.go('materiais.professor');
-                }
+          url: '/disciplinas/:disciplinaID/materiais',
+          templateUrl: '/partials/materiais',
+          controller: function ($cookies, $state) {
+            if ($cookies.get('profile') == 0) {
+              $state.go('materiais.aluno')
+            } else {
+              $state.go('materiais.professor')
             }
+          },
         })
             .state('materiais.aluno', {
-                url: "/aluno",
-                templateUrl:'partials/materiais-a.html',
-                controller: "DisciplinaController"
+              url: '/aluno',
+              templateUrl: '/partials/materiais-a',
+              controller: 'DisciplinaController',
             })
             .state('materiais.professor', {
-                url: "/professor",
-                templateUrl:'partials/materiais-p.html',
-                controller: "DisciplinaController"
+              url: '/professor',
+              templateUrl: '/partials/materiais-p',
+              controller: 'DisciplinaController',
             })
         .state('participantes', {
-            url: "/disciplinas/:disciplinaID/participantes",
-            templateUrl: "partials/participantes.html",
-            controller: 'DisciplinaController'
+          url: '/disciplinas/:disciplinaID/participantes',
+          templateUrl: '/partials/participantes',
+          controller: 'DisciplinaController',
         })
         .state('duvidas', {
-            url: "/aulas/:aulaID",
-            templateUrl:"partials/duvidas.html"
+          url: '/aulas/:aulaID',
+          templateUrl: '/partials/duvidas',
         })
             .state('duvidas.aluno', {
-                url: "/aluno",
-                templateUrl:'partials/duvidas-a.html',
-                controller: "AulaController"
+              url: '/aluno',
+              templateUrl: '/partials/duvidas-a',
+              controller: 'AulaController',
             })
             .state('duvidas.professor', {
-                url: "/professor",
-                templateUrl:'partials/duvidas-p.html',
-                controller: "AulaController"
+              url: '/professor',
+              templateUrl: '/partials/duvidas-p',
+              controller: 'AulaController',
             })
-        .state('material-aula',  {
-            url: "/aulas/:aulaID/material",
-            templateUrl:"partials/material-aula.html",
-            controller: function($cookies, $state) {
-                if($cookies.get('profile') == 0) {
-                    $state.go('material-aula.aluno');
-                } else {
-                    $state.go('material-aula.professor');
-                }
+        .state('material-aula', {
+          url: '/aulas/:aulaID/material',
+          templateUrl: '/partials/material-aula',
+          controller: function ($cookies, $state) {
+            if ($cookies.get('profile') == 0) {
+              $state.go('material-aula.aluno')
+            } else {
+              $state.go('material-aula.professor')
             }
+          },
         })
             .state('material-aula.aluno', {
-                url: "/aluno",
-                templateUrl:'partials/material-aula-a.html',
-                controller: 'AulaController'
+              url: '/aluno',
+              templateUrl: '/partials/material-aula-a',
+              controller: 'AulaController',
             })
             .state('material-aula.professor', {
-                url: "/professor",
-                templateUrl:'partials/material-aula-p.html',
-                controller: 'AulaController'
+              url: '/professor',
+              templateUrl: '/partials/material-aula-p',
+              controller: 'AulaController',
             })
-}).run(function($window, $location, $state) {
-    if($window.location.pathname == '/home')
-        $state.go('disciplinas');
-});
+}).run(function ($window, $location, $state) {
+  if ($window.location.pathname == '/home')
+    $state.go('disciplinas')
+})
