1a292af751bf58529bb790139eb5c7ba81140754
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2016-08-29 17:03:30 -0300

    Add doctype to partials and Fix errors related to the new session cookie format

--- public/javascripts/angular/controllers/DisciplinasController.js
+++ public/javascripts/angular/controllers/DisciplinasController.js
@@ -1,46 +1,46 @@
 oddin.controller('DisciplinasController',
-  function($scope, Disciplina, $cookies, Profile, $stateParams, $state) {
+  function ($scope, Disciplina, $cookies, Profile, $stateParams, $state) {
     function buscaDisciplinas() {
       Disciplina.query(
-        function(disciplinas) {
-          $scope.disciplinas = disciplinas;
+        function (disciplinas) {
+          $scope.disciplinas = disciplinas
         },
-        function(erro) {
-          console.log("Não foi possível obter a lista de disciplinas");
-          console.log(erro);
+        function (erro) {
+          console.log('Não foi possível obter a lista de disciplinas')
+          console.log(erro)
         }
-      );
+      )
     }
-    $scope.buscarPerfil = function(disciplina) {
-        if(!$cookies.get('profile')) {
-            Profile.get({id: disciplina.id},
-                function(data) {
-                    $cookies.put('profile', data.profile);
-                    if($cookies.get('profile') == 0) {
-                        $state.go('aulas.aluno', {'disciplinaID': disciplina.id});
-                    } else {
-                        $state.go('aulas.professor', {'disciplinaID': disciplina.id});
-                    }
+    $scope.buscarPerfil = function (disciplina) {
+      if (!$cookies.get('profile')) {
+        Profile.get({ id: disciplina.id },
+                function (data) {
+                  $cookies.put('profile', data.profile)
+                  if ($cookies.get('profile') == 0) {
+                    $state.go('aulas.aluno', { 'disciplinaID': disciplina.id })
+                  } else {
+                    $state.go('aulas.professor', { 'disciplinaID': disciplina.id })
+                  }
                 },
-                function(erro) {
-                    console.log("Erro ao encontrar perfil");
+                function (erro) {
+                  console.log('Erro ao encontrar perfil')
                 }
-            );
+            )
+      } else {
+        if ($cookies.get('profile') == 0) {
+          $state.go('aulas.aluno', { 'disciplinaID': disciplina.id })
         } else {
-            if($cookies.get('profile') == 0) {
-                $state.go('aulas.aluno', {'disciplinaID': disciplina.id});
-            } else {
-                $state.go('aulas.professor', {'disciplinaID': disciplina.id});
-            }
+          $state.go('aulas.professor', { 'disciplinaID': disciplina.id })
         }
+      }
     }
 
     $scope.usuario = {
-        'nome': JSON.parse($cookies.get('session').substring(2)).person.name,
-        'email': JSON.parse($cookies.get('session').substring(2)).user.email
+      'nome': JSON.parse($cookies.get('session').substring(2)).person.name,
+      'email': JSON.parse($cookies.get('session').substring(2)).person.email,
     }
 
-    $scope.titulo = "Disciplinas";
-    buscaDisciplinas();
+    $scope.titulo = 'Disciplinas'
+    buscaDisciplinas()
   }
-);
+)
