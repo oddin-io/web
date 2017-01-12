694a5fee810862745b39cf58232bfc398075590d
Heitor Kuba <hjkuba@yahoo.com.br>
2017-01-12 12:54:58 -0200

    Structuring Survey angular controller

--- public/javascripts/angular/controllers/SurveysController.js
+++ public/javascripts/angular/controllers/SurveysController.js
@@ -4,23 +4,23 @@ oddin.controller('SurveysController', function ($http, $scope, $stateParams, $st
 			'email': JSON.parse($cookies.get('session').substring(2)).person.email,
 	}
 	$scope.data_loaded = true;
-
+	$scope.survey = {
+		choices : [{}]
+	}
 	$scope.surveys = [
 		{
-			id: 1,
+			id: 1, //criado no backend
 			title: "Enquete 1",
-			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dapibus mauris. Maecenas tristique, dolor vel fringilla accumsan, ipsum tellus tempor massa, vel ornare lorem nulla ut quam. Sed at risus quis purus convallis aliquam at et leo. Aliquam ullamcorper, sapien facilisis dapibus maximus, dolor ante viverra risus, at hendrerit risus magna eu urna. Integer ullamcorper, nulla vel tempus gravida, urna sapien eleifend massa, a accumsan ex sem bibendum tortor. Nam fringilla metus in orci pulvinar, at tristique turpis tincidunt. Aliquam eu suscipit ante. Teste blah blah blah blah",
-			created_at: "12/12/2012",
-			answers: [
+			question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel dapibus mauris. Maecenas tristique, dolor vel fringilla accumsan, ipsum tellus tempor massa, vel ornare lorem nulla ut quam. Sed at risus quis purus convallis aliquam at et leo. Aliquam ullamcorper, sapien facilisis dapibus maximus, dolor ante viverra risus, at hendrerit risus magna eu urna. Integer ullamcorper, nulla vel tempus gravida, urna sapien eleifend massa, a accumsan ex sem bibendum tortor. Nam fringilla metus in orci pulvinar, at tristique turpis tincidunt. Aliquam eu suscipit ante. Teste blah blah blah blah",
+			created_at: "12/12/2012", //criado no backend
+			choices: [
 				{
-					id: 1,
-					alternative: "a",
-					voteCount: 10,
+					id: 1, //criado no backend
+					voteCount: 10, //criado no backend
 					text: "Lorem Ipsum",
 				},
 				{
 					id: 2,
-					alternative: "b",
 					voteCount: 8,
 					text: "Sit Dolor",
 				},
@@ -29,26 +29,26 @@ oddin.controller('SurveysController', function ($http, $scope, $stateParams, $st
 		{
 			id: 2,
 			title: "Enquete 2",
-			description: "Question 2",
-			answer: "Answer 2"
+			question: "Question 2",
+			choices: "Answer 2"
 		},
 		{
 			id: 3,
 			title: "Enquete 3",
-			description: "Question 3",
-			answer: "Answer 3"
+			question: "Question 3",
+			choices: "Answer 3"
 		},
 		{
 			id: 4,
 			title: "Enquete 4",
-			description: "Question 4",
-			answer: "Answer 4"
+			question: "Question 4",
+			choices: "Answer 4"
 		},
 		{
 			id: 5,
 			title: "Enquete 5",
 			description: "Question 5",
-			answer: "Answer 5"
+			choices: "Answer 5"
 		}
 	];
 
@@ -78,4 +78,36 @@ oddin.controller('SurveysController', function ($http, $scope, $stateParams, $st
 			return "Ocultar Respostas"
 		}
 	}
+
+	$scope.createSurvey = function () {
+		console.log($scope.survey);
+		$scope.survey = {
+			choices : [{}]
+		}
+	}
+
+	$scope.addNewChoice = function () {
+		// var newItemNo = $scope.choices.length+1;
+		$scope.survey.choices.push({});
+	}
+
+	$scope.removeChoice = function() {
+	 var lastItem = $scope.survey.choices.length-1;
+	 $scope.survey.choices.splice(lastItem);
+ };
+
+ $scope.openModalDeleteSurvey = function (survey) {
+	 $scope.modalContent = angular.copy(survey);
+	 $('#delete-enquete').openModal();
+ }
+
+ $scope.openModalCloseSurvey = function (survey) {
+	 $scope.modalContent = angular.copy(survey);
+	 $('#close-enquete').openModal();
+ }
+
+ $scope.openModalEditSurvey = function (survey) {
+	 $scope.modalContent = angular.copy(survey);
+	 $('#edit-enquete').openModal();
+ }
 });
