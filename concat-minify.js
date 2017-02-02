b2a10fcc9a8862e091fddd0d186dc1fb9f6ad51e
Heitor Kuba <hjkuba@yahoo.com.br>
2017-02-02 11:43:51 -0200

    Set minified environments for production

--- concat-minify.js
+++ concat-minify.js
@@ -3,10 +3,61 @@ const compressor = require('node-minify')
 compressor.minify({
   compressor: 'uglifyjs',
   input: 'public/javascripts/**/*.js',
-  output: 'public/build/oddin-build.min.js',
+  output: 'public/build/oddin-home-build.min.js',
   callback: function(err, min) {
-    console.log('wildcards Uglifyjs');
-    console.log(err);
-    //console.log(min);
+		if(err) {
+    	console.log(err);
+			return;
+		}
+    console.log("- home-build-js minified");
+  }
+});
+
+compressor.minify({
+  compressor: 'uglifyjs',
+  input: ['public/javascripts/angular/app.js',
+	'public/javascripts/angular/config.js',
+	'public/javascripts/angular/controllers/LoginController.js',
+	'public/javascripts/angular/services/LoginAPI.js',
+	'public/javascripts/angular/directives/oddinPreloader.js'],
+  output: 'public/build/oddin-index-build.min.js',
+  callback: function(err, min) {
+		if(err) {
+    	console.log(err);
+			return;
+		}
+    console.log("- index-build-js minified");
+  }
+});
+
+compressor.minify({
+  compressor: 'clean-css',
+  input: ['public/stylesheets/material-icons.css',
+	'public/stylesheets/materialize_custom.css',
+	'public/stylesheets/login_style.css'
+	],
+  output: 'public/build/oddin-index-build.css',
+  callback: function(err, min) {
+		if(err) {
+    	console.log(err);
+			return;
+		}
+    console.log("- index-build-css minified");
+  }
+});
+
+compressor.minify({
+  compressor: 'clean-css',
+  input: ['public/stylesheets/material-icons.css',
+	'public/stylesheets/materialize_custom.css',
+	'public/stylesheets/user_style.css'
+	],
+  output: 'public/build/oddin-home-build.css',
+  callback: function(err, min) {
+		if(err) {
+    	console.log(err);
+			return;
+		}
+    console.log("- home-build-css minified");
   }
 });
--- config/constants.js
+++ config/constants.js
@@ -1,3 +1,4 @@
 module.exports = {
   uri: (process.env.WS_URL) || 'http://ws-edupanel.herokuapp.com',
+	envMode: (process.env.MODE) || 'test-env'
 }
