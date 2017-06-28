40a099dd5a186af58c7d90ad6a53621898af5813
Bruno Felipe Leal Delfino <bruno.delfino1995@gmail.com>
2017-06-28 00:23:52 -0300

    Import Angular addons as JS Modules

--- public/javascripts/angular/app.js
+++ public/javascripts/angular/app.js
@@ -1,4 +1,10 @@
-const oddin = angular.module('oddin', ['ui.router', 'ui.materialize', 'ngCookies', 'ui.mask'])
+import angular from 'angular'
+import uiRouter from 'angular-ui-router'
+import uiMask from 'angular-ui-mask'
+import ngMaterialize from 'angular-materialize'
+import ngCookies from 'angular-cookies'
+
+const oddin = angular.module('oddin', [uiRouter, ngMaterialize, ngCookies, uiMask])
 oddin.constant('env', window.env)
 
 export default oddin
